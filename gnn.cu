32dacdcf1177da53049cb70d875b9b09390cc56a
Tirth (Windows) <tirth11804@gmail.com>
2026-09-11 21:31:07 -0400

    ported MatFill()

--- gnn.cu
+++ gnn.cu
@@ -1,3 +1,17 @@
+#define GNN_IMPLEMENTATION
+#include "gnn.cuh"
+
+float d[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 int main() {
-    //stub
+    Mat m = {
+        .rows = 3,
+        .cols = 3,
+        .stride = 3,
+        .es = d,
+    };
+
+    MAT_PRINT(m);
+    MatFill(&m, 1);
+    MAT_PRINT(m);
 }
--- gnn.cuh
+++ gnn.cuh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdio>
+
 typedef struct {
     size_t rows;
     size_t cols;
@@ -8,18 +10,51 @@ typedef struct {
 } Mat;
 
 #define MAT_AT(m, i, j) (m).es[(i)*(m).stride + (j)]
+#define MAT_PRINT(m) mat_print(m, #m)
 
-void MatFill(Mat m, float n);
 __global__ void MatFillKernel(Mat m, float n);
+void MatFill(Mat* m, float n);
+__global__ void MatRandKernel(Mat m, float low, float high);
+void MatRand(Mat* m, float low, float high);
 
 #ifdef GNN_IMPLEMENTATION
 
-void MatFill(Mat m, float n) {
-    //stub
+__global__ void MatFillKernel(Mat m, float n) {
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    MAT_AT(m, tx, ty) = n;
 }
 
-__global__ void MatFillKernel(Mat m, float n) {
-    //stub
+void MatFill(Mat* m, float n) {
+    float* d_es;
+    int size = m->rows * m->cols * sizeof(float);
+    int width = m->stride;
+
+    cudaMalloc((void**)&d_es, size);
+    Mat d_mat = *m;
+    d_mat.es = d_es;
+
+    dim3 dimGrid(1, 1);
+    dim3 dimBlock(width, width);
+
+    MatFillKernel<<<dimGrid, dimBlock>>>(d_mat, n);
+
+    cudaMemcpy(m->es, d_es, size, cudaMemcpyDeviceToHost);
+    cudaFree(d_es);
+}
+
+__global__ void MatRandKernel(Mat m, float low, float high) {}
+void MatRand(Mat* m, float low, float high) {}
+
+void mat_print(Mat m, const char *name) {
+    printf("%s\n", name);
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            printf("    %f ", MAT_AT(m, i, j));
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 #endif //GNN_IMPLEMENTATION
