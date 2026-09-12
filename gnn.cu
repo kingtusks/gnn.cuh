2152fdc0e9e96283d5ec5fdbd1471e6cbcc359e0
Tirth (Windows) <tirth11804@gmail.com>
2026-09-11 23:24:03 -0400

    added a mat_copy function

--- gnn.cu
+++ gnn.cu
@@ -3,4 +3,10 @@
 
 int main() {
     Mat m = mat_alloc(3, 3);
+    mat_fill(m, 2);
+    MAT_PRINT(m);
+
+    Mat dst = mat_alloc(3, 3);
+    mat_copy(dst, m);
+    MAT_PRINT(dst);
 }
--- gnn.cuh
+++ gnn.cuh
@@ -2,6 +2,11 @@
 
 #include <cstdio>
 
+#ifndef GNN_ASSERT
+#include <assert.h>
+#define GNN_ASSERT assert
+#endif //GNN_ASSERT
+
 typedef struct {
     size_t rows;
     size_t cols;
@@ -13,11 +18,16 @@ typedef struct {
 #define MAT_PRINT(m) mat_print(m, #m)
 
 Mat mat_alloc(size_t rows, size_t cols);
+
 __global__ void mat_fill_kernel(Mat m, float n);
-void mat_fill(Mat* m, float n);
+void mat_fill(Mat m, float n);
+
 Mat mat_row(Mat m, size_t row);
-// __global__ void MatRandKernel(Mat m, float low, float high);
-// void MatRand(Mat* m, float low, float high);
+
+__global__ void mat_copy_kernel(Mat dst, Mat m);
+void mat_copy(Mat dst, Mat m);
+
+void mat_print(Mat m, const char *name);
 
 #ifdef GNN_IMPLEMENTATION
 
@@ -36,10 +46,10 @@ __global__ void mat_fill_kernel(Mat m, float n) {
     MAT_AT(m, tx, ty) = n;
 }
 
-void mat_fill(Mat* m, float n) {
+void mat_fill(Mat m, float n) {
     dim3 dimGrid(1, 1);
-    dim3 dimBlock(m->rows, m->cols);
-    mat_fill_kernel<<<dimGrid, dimBlock>>>(*m, n);
+    dim3 dimBlock(m.rows, m.cols);
+    mat_fill_kernel<<<dimGrid, dimBlock>>>(m, n);
 }
 
 Mat mat_row(Mat m, size_t row) {
@@ -51,8 +61,17 @@ Mat mat_row(Mat m, size_t row) {
     };
 }
 
-// __global__ void MatRandKernel(Mat m, float low, float high) {}
-// void MatRand(Mat* m, float low, float high) {}
+__global__ void mat_copy_kernel(Mat dst, Mat m) {
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    MAT_AT(dst, tx, ty) = MAT_AT(m, tx, ty);
+}
+
+void mat_copy(Mat dst, Mat m) {
+    dim3 dimGrid(1, 1);
+    dim3 dimBlock(m.rows, m.cols);
+    mat_copy_kernel<<<dimGrid, dimBlock>>>(dst, m);
+}
 
 void mat_print(Mat m, const char *name) {
     float* host_es = (float*)malloc(m.rows * m.stride * sizeof(float));
